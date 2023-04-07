8579526bd5de1c7d36ef5bc9c0be7dba4aa403d9
Nehaa7255 <nehamas04@gmail.com>
2023-04-07 22:38:52 +0530

    Payment_gateway app

---
+++ paymentApp/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const Razorpay = require('razorpay');
+
+var instance = new Razorpay({
+  key_id: 'rzp_test_r6EJwrxJbscSv9',
+  key_secret: 'A0ETOcXCfUXgupemi1uCptXr',
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/create/orderID',(req,res)=>{
+  var options = {
+    amount: 50000,  // amount in the smallest currency unit
+    currency: "INR",
+    receipt: "order_rcptid_11"
+  };
+  instance.orders.create(options, function(err, order) {
+    console.log(order);
+    return res.send(order)
+  });
+})
+
+
+module.exports = router;
